2b761fce2b9e4f9d80975e2b42a7e6200ba2524e
yujianbohz <yujianbosd@163.com>
2017-07-08 12:24:33 +0800

    port

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,6 @@ module.exports = {
    devServer: {
     contentBase: "./public",//本地服务器所加载的页面所在的目录
     historyApiFallback: true,//不跳转
-    inline: true,//实时刷新
-	port:8080
+    inline: true//实时刷新
   } 
 }
